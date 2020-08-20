6f8d62afe93549064bd18fa99acb1ce95b48d207
Ammon Burgi <ammonburgi@Ammons-MacBook-Pro.local>
2020-08-19 20:40:37 -0600

    footer

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './footer.css'
+
+function Footer(){
+    return(
+        <footer>
+            <span>
+                <p>Advertising</p>
+                <p>Business</p>
+                <p>How Search Works</p>
+            </span>
+            <span>
+                <p>Privacy</p>
+                <p>Terms</p>
+                <p>Settings</p>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
