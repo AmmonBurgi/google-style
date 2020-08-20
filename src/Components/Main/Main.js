c07d694f0d1ad985b0b47540f4090ce876ac1184
Ammon Burgi <ammonburgi@Ammons-MacBook-Pro.local>
2020-08-19 20:56:52 -0600

    main

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './main.css'
+
+function Main(){
+    return(
+        <section className='main'>
+            <img />
+            <input />
+            <span className='main-buttons'>
+                <button>Google Search</button>
+                <button>I'm Feeling Lucky</button>
+            </span>
+        </section>
+    )
+}
+
+export default Main
