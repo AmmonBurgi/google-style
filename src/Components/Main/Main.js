5869c8060773defaef7b26f62bfca3a9a7327ef2
Ammon Burgi <ammonburgi@Ammons-MacBook-Pro.local>
2020-09-16 00:09:27 -0600

    completed styling

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -2,15 +2,16 @@ import React from 'react'
 import './main.css'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faMicrophone, faSearch} from '@fortawesome/free-solid-svg-icons'
+import google from '../../google.png'
 
 function Main(){
     return(
         <section className='main'>
-            <img alt='theImage' />
+            <img className='google-image' alt='google' src={google} />
             <div className='input-section'>
-                <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon>
+                <FontAwesomeIcon className='input-icon' icon={faSearch}></FontAwesomeIcon>
                 <input className='inner-input' />
-                <FontAwesomeIcon icon={faMicrophone}></FontAwesomeIcon>
+                <FontAwesomeIcon className='input-icon' icon={faMicrophone}></FontAwesomeIcon>
             </div>
             <span className='main-buttons'>
                 <button>Google Search</button>
